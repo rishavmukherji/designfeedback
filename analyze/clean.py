9f2f2abf317e77dcf591acc9f791ade27b9ce094
Andrew Mauboussin <ammaub@college.harvard.edu>
2014-12-02 17:11:57 -0500

    added comments csv

--- analyze/clean.py
+++ analyze/clean.py
@@ -5,12 +5,17 @@ from os import listdir
 clean = lambda s : ''.join([c for c in s.strip() if c.isalpha() or c == ' '])
 
 results_file = 'results.csv'
+exclude = ['means.csv', 'comments.csv']
 
-files = [f for f in listdir('.') if f.endswith('.csv') and f != results_file]
+files = [f for f in listdir('.') if f.endswith('.csv') and f != results_file and f not in exclude]
 
 wrows = [['site', 'question', 'category', 'answer']]
 
+site_comments = {}
+
 for f in files:
+	comments = []
+
 	qlookup = hits[f.replace('.csv', '')]
 	questions = [(q, get_cat(question_ids[q.lower()])) for q in qlookup] 
 	with open(f, 'r') as rfile:
@@ -22,9 +27,19 @@ for f in files:
 				a = int(a[1]) if len(a[1]) > 0 else 0
 
 				wrows.append([clean(f.replace('.csv', '')), q[0], q[1], a ])
+			
+			if 'Answer.comments' in row and row['Answer.comments']:
+				comments.append(row['Answer.comments'])
+	site_comments[f.replace('.csv', '')] = comments
 
-with open(results_file ,'w') as wfile:
+with open('comments.csv', 'w') as wfile:
 	writer = csv.writer(wfile)
-	for r in wrows:
-		writer.writerow(r)
+	for k, all_comments in site_comments.items():
+		for c in all_comments:
+			writer.writerow([k, c])
+
+# with open(results_file ,'w') as wfile:
+# 	writer = csv.writer(wfile)
+# 	for r in wrows:
+# 		writer.writerow(r)
 
