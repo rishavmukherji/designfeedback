57bddcc3eb956e5ff63e87ccbeb45dd8902bd3e6
Andrew Mauboussin <ammaub@college.harvard.edu>
2014-12-11 21:49:17 -0500

    worked on comment ranking and other things

--- analyze2/rank.py
+++ analyze2/rank.py
@@ -1,10 +1,32 @@
-import csv
+import csv, json
+from collections import defaultdict
+
+names = {
+	'Aesthetic and Minimalist Design' : 'Aesthetic',
+	'Consistency and Standard' : 'Consistency',
+	'Error Prevention' : 'Error Prevention',
+	'Help Users Recognize, Diagnose, and Recover from Errors' : 'Error Recovery',
+	'Help and Documentation' : 'Documentation',
+	'Match between System and the Real World' : 'Match to Real World',
+	'Recognition rather than recall' : 'Recognition',
+	'User control and Freedom' : 'User Control',
+	'Visibility of System Status' : 'Visibility' 
+	}
 
 with open('results2.csv', 'r') as rfile:
 	reader = csv.DictReader(rfile)
-
 	rows = [r for r in reader]
 
-rows = sorted(rows, key = lambda r : len(r['comment']), reverse = True )
 
-print '\n'.join(map(lambda r : r['comment'], rows))
+sites = defaultdict(list)
+for r in rows:
+	r['site'] = r['site'][1:].replace('.csv', '')
+	sites[r['site']].append(r)
+
+
+for site, rows in sites.items():
+	rows = sorted(rows, key = lambda r : len(r['comment']), reverse = True )
+	print site
+	print json.dumps(rows)
+
+#print '\n'.join(map(lambda r : r['comment'], rows))
--- frontend/make_json2.py
+++ frontend/make_json2.py
@@ -4,11 +4,45 @@ from scipy.stats import sem
 from collections import defaultdict
 from questions import *
 
-def verification():
-	with open('../analyze/worker_scores.csv', 'r') as rfile:
+def main():
+	verification()
 
-	
+vquestions = {
+	'conference-q1' : 'Who is the president of the Executive Board?',
+	'conference-q2' : 'When was the last event organized by this organization?',
+	'conference-q4' : 'Try and register for the upcoming conference in India. What is the "Form Name" shown when you try to register? (Don\'t worry, your registrations for the conference won\'t be accepted):',
+
+	'scratchpad-q1' : 'Who is the faculty advisor of the Harvard Cricket Club?',
+	'scratchpad-q2' :'How many people are listed in the alumni section of the club?',
+	'scratchpad-q4' :'Who scored more runs in the Harvard vs UPenn game on Saturday, Sep 21?',
+
+	'andrew-q1' : 'Who is the publisher for The Success Equation',
+	'andrew-q2' : 'What email address should send any questions or feedback relating to the site to?',
+	'andrew-q4' : 'If player 1 has a 51% chance of winning one point, what is the probability they will win a five set match (use the tennis simulation page)?',
+}
+
+def verification():
+	with open('../analyze/verification_results.csv', 'r') as rfile:
+		vreader = csv.DictReader(rfile)
+		rows = [r for r in vreader]
+
+	questions = defaultdict(list)
+	for r in rows:
+		questions[r['site'] + '-' + r['question']].append(int(r['correct']))
+
+	aggregate = defaultdict(list)
+
+	for q,responses in questions.items():
+
+		aggregate[q.split('-')[0]].append({'question' : vquestions[q], 'correct' : float('%.2f' % np.mean(responses)), 'total' : len(responses)})
+
+	print json.dumps(aggregate)
+
+
+
 def first_chart():
+
+	min_score = 1
 
 	scores = {}
 	with open('../analyze/worker_scores.csv', 'r') as rfile:
@@ -35,7 +69,7 @@ def first_chart():
 				hit_worker = r['site'] + str(r['hit']) + '-' + r['worker']
 				score = scores[hit_worker] if hit_worker in scores else 0
 
-				if score >= 1:
+				if score >= min_score:
 					questions[r['question']].append(int(r['answer']))
 					categories[r['question']] = r['category']
 
@@ -69,13 +103,7 @@ def first_chart():
 
 		
 
-
-
-
-
-
-
-
+main()
 
 
 
